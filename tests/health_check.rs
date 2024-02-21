4a5a53694d2233ccb5cede22dfd81d86adcdf481
Pascal <pascalbehmenburg@posteo.net>
2024-02-21 20:27:26 +0100

    refactor health_check

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,9 +2,10 @@ use rs_fullstack::run;
 use rstest::{fixture, rstest};
 use std::net::TcpListener;
 
-/// Used to spawn a backend app for each test without having to explicitly use an async context.
+/// Used to spawn a backend app and receive its address for each
+/// test without having to explicitly use an async context.
 #[fixture]
-fn spawn_backend() -> String {
+fn backend_address() -> String {
     // port 0 will be resolved by the os which will return a somewhat random port which is not in use.
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
@@ -21,11 +22,11 @@ fn spawn_backend() -> String {
     format!("http://127.0.0.1:{}", port)
 }
 
-#[rstest(spawn_backend as address)]
+#[rstest]
 #[tokio::test]
-async fn health_check_works(address: String) {
+async fn health_check_works(backend_address: String) {
     let response = reqwest::Client::new()
-        .get(&format!("{}/health_check", &address))
+        .get(&format!("{}/health_check", &backend_address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -34,11 +35,11 @@ async fn health_check_works(address: String) {
     assert_eq!(Some(0), response.content_length());
 }
 
-#[rstest(spawn_backend as address)]
+#[rstest]
 #[tokio::test]
-async fn v1_users_register_is_200_for_valid_data(address: String) {
+async fn v1_users_register_is_200_for_valid_data(backend_address: String) {
     let response = reqwest::Client::new()
-        .post(&format!("{}/users/register", &address))
+        .post(&format!("{}/users/register", &backend_address))
         .header("Content-Type", "application/json")
         .body(
             "{\"data\": {\"name\": \"test\", \"email\": \"test@test.test\", \"password\": \"test\"}}",
@@ -50,7 +51,7 @@ async fn v1_users_register_is_200_for_valid_data(address: String) {
     assert_eq!(200, response.status().as_u16());
 }
 
-#[rstest(spawn_backend as address)]
+#[rstest]
 #[case::missing_email_and_password("{\"data\": {\"name\": \"test\"}}")]
 #[case::missing_name_and_password("{\"data\": {\"email\": \"test%40test.test\"}}")]
 #[case::missing_name_and_email("{\"data\": {\"password\": \"test\"}}")]
@@ -59,9 +60,12 @@ async fn v1_users_register_is_200_for_valid_data(address: String) {
 #[case::missing_name("{\"data\": {\"password\": \"test\", \"email\": \"test%40test.test\"}}")]
 #[case::missing_all_fields("")]
 #[tokio::test]
-async fn v1_users_register_is_400_for_missing_data(address: String, #[case] invalid_body: String) {
+async fn v1_users_register_is_400_for_missing_data(
+    backend_address: String,
+    #[case] invalid_body: String,
+) {
     let response = reqwest::Client::new()
-        .post(&format!("{}/users/register", &address))
+        .post(&format!("{}/users/register", &backend_address))
         .header("Content-Type", "application/json")
         .body(invalid_body)
         .send()
